a6aa2775361206be98f2c4ae2960fed15ad1f2a4
Goinza <gonzalo-am@hotmail.com>
2020-05-18 16:54:08 -0300

    Update _config.js

--- Weapons and Items as Original Data/_config.js
+++ Weapons and Items as Original Data/_config.js
@@ -3,5 +3,5 @@
 var SpellsConfig = {
     ADD_WINDOW: true, //If it is true, the spells window will be added to the unit menu screen. Else, it won't be added
     NAME: "Spells", //Name for the category of weapons and items used as original data. Used by the spells window
-    ORIGINAL_DATA_TAB: 1
+    ORIGINAL_DATA_TAB: 0 //Tab used for the Original Data entries. Keep in mind that the first tab is 0, the second is 1, and so on.
 }
