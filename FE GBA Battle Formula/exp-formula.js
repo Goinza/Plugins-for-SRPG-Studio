584bfba21d81f9194ae0df838d9289a7cddf763f
D-MAS <57054077+D-MAS@users.noreply.github.com>
2020-12-31 07:55:07 -0600

    Update exp-formula.js
    added vars for nodamage, class bonuses & penalties, now uses config for boss exp

--- FE GBA Battle Formula/exp-formula.js
+++ FE GBA Battle Formula/exp-formula.js
@@ -7,22 +7,28 @@
     Class Bonus B = 0/60
     Boss Bonus = 40
     */
+	var ExperienceNoDamageMult = 0; // 0
+	var ExperienceNormalHighBonus = 20; // 20
+	var ExperienceNormalHighPenal = 20; // 20
+	var ExperienceVictoryHighBonus = 20; // 20
+	var ExperienceVictoryHighPenal = 20; // 20
 
     //Always 1
     ExperienceCalculator._getNoDamageExperience = function(data) {
-        return this._getValidExperience(1);
+        //return this._getValidExperience(1);
+		return (ExperienceNoDamageMult * ExperienceCalculator._getNormalValue(data));
     }
 
     //[31 + (enemy's Level + enemy's Class bonus A) - (Level + Class bonus A)] / Class power    
     ExperienceCalculator._getNormalValue = function(data) {
         var passiveLv = data.passive.getLv();
         var passiveClass = data.passive.getClass();
-        var passiveBonus = passiveClass.getClassRank() == ClassRank.LOW ? 0 : 20;
+        var passiveBonus = passiveClass.getClassRank() == ClassRank.LOW ? 0 : ExperienceNormalHighBonus;
         var activeLv = data.active.getLv();
         var activeClass = data.active.getClass();
-        var activeBonus = activeClass.getClassRank() == ClassRank.LOW ? 0 : 20;
+        var activeBonus = activeClass.getClassRank() == ClassRank.LOW ? 0 : ExperienceNormalHighPenal;
 
-        var finalExp = (31 + (passiveLv + passiveBonus) - (activeLv + activeBonus)) / 3;
+        var finalExp = ((this._getBaseExperience() + 1) + (passiveLv + passiveBonus) - (activeLv + activeBonus)) / 3;
 
         return this._getValidExperience(finalExp);
     }
@@ -32,13 +38,15 @@
     ExperienceCalculator._getVictoryExperience = function(data) {
         var passiveLv = data.passive.getLv();
         var passiveClass = data.passive.getClass();
-        var passiveBonus = passiveClass.getClassRank() == ClassRank.LOW ? 0 : 60;
+        var passiveBonus = passiveClass.getClassRank() == ClassRank.LOW ? 0 : ExperienceVictoryHighBonus;
         var activeLv = data.active.getLv();
         var activeClass = data.active.getClass();
-        var activeBonus = activeClass.getClassRank() == ClassRank.LOW ? 0 : 60;
+        var activeBonus = activeClass.getClassRank() == ClassRank.LOW ? 0 : ExperienceVictoryHighPenal;
 
-        var baseExp = (passiveLv * 3 + passiveBonus) - (activeLv * 3 + activeBonus);
-        var bossBonus = data.passive.getImportance() == ImportanceType.LEADER ? 20 : 0;
+        // var baseExp = (passiveLv * 3 + passiveBonus) - (activeLv * 3 + activeBonus);
+        var baseExp = (passiveLv - activeLv) * 3;
+        // var bossBonus = data.passive.getImportance() == ImportanceType.LEADER ? 20 : 0;
+		var bossBonus = this._getBonusExperience(data.passive);
         var aux = baseExp + 20 + bossBonus;
         aux = aux>0 ? aux : 0;
 
